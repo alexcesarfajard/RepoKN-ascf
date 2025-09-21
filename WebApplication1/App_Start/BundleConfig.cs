1da808d6022b36cc6771623748414298eef4dff3
alexcesarfajard <acesar23616@ufide.ac.cr>
2025-09-21 12:38:06 -0600

    Diseño y vista de login y register

--- WebApplication1/App_Start/BundleConfig.cs
+++ WebApplication1/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace WebApplication1
 {
@@ -8,7 +7,7 @@ namespace WebApplication1
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            
+
         }
     }
 }
