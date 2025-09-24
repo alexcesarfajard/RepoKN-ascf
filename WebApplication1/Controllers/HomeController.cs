f240a513a31cf5e5d8dd3cad738eab61d92f87e7
alexcesarfajard <acesar23616@ufide.ac.cr>
2025-09-23 20:52:14 -0600

    Progra inicial de login

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,24 +1,69 @@
 ﻿using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        #region Iniciar Sesion
+
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Index(Usuario usuario)
+        {
+
+            /*Progra para validar si usuario.CorreoElectronico y usuario.Contrasenna son válidas */
+
+            return RedirectToAction("Principal", "Home");
+        }
+
+        #endregion
+
+        #region Registro de usuario
+
+        [HttpGet]
         public ActionResult Registro()
         {
             return View();
         }
 
-        public ActionResult RecuperarAcceso() 
+        [HttpPost]
+        public ActionResult Registro(Usuario usuario)
+        {
+            /*Progra para guardar un usuario en la BD */
+
+            return View();
+        }
+
+        #endregion
+
+        #region Recuperar Acceso 
+
+        [HttpGet]
+        public ActionResult RecuperarAcceso()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult RecuperarAcceso(Usuario usuario) 
+        {
+
+            /*Validar si el usuario existe*/
+            /*Generar una contraseña temporal*/
+            /*Enviar esa contraseña temporal para iniciar sesion*/
+
+            return View();
+        }
+
+        #endregion
+
+        [HttpGet]
         public ActionResult Principal()
         {
             return View();
---
+++ WebApplication1/Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Usuario
+    {
+        public string Identificacion { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string CorreoElectronico { get; set; }
+
+        public string Contrasenna { get; set; }
+
+
+    }
+}
